10297edb9eb546288850527c252ecebb241393e3
Kevin Sung <kevin.sung.123@gmail.com>
2019-07-15 11:17:05 -0400

    Updates

---
+++ precache-manifest.16f16c6c7b78b302b1d97dafe2985814.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ee0a2681c176d93531b1a098a7455db",
+    "url": "/solarpixdev/static/media/categoryListFile_salessv.0ee0a268.bin"
+  },
+  {
+    "revision": "1ad945962141fc1462abe9ebfc1e5857",
+    "url": "/solarpixdev/static/media/categoryListFile_pcsv.1ad94596.bin"
+  },
+  {
+    "revision": "d1a472a9c02578281e51ef9fca943308",
+    "url": "/solarpixdev/static/media/categoryListFile_install.d1a472a9.bin"
+  },
+  {
+    "revision": "c8c4ccc7647a1b026faf",
+    "url": "/solarpixdev/static/js/runtime~main.e2626c19.js"
+  },
+  {
+    "revision": "e05d371f49640dde247d",
+    "url": "/solarpixdev/static/js/main.54a0f8b7.chunk.js"
+  },
+  {
+    "revision": "12670af83868b845de7b",
+    "url": "/solarpixdev/static/js/2.195e5fcc.chunk.js"
+  },
+  {
+    "revision": "ddc27ce96fb822a0200f1aa042add18b",
+    "url": "/solarpixdev/index.html"
+  }
+];
